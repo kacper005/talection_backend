75bfe623b59fe23576725d1addc855acafeef15e
Kristian <garderkristian@gmail.com>
2025-06-09 10:02:57 +0200

    Initialized spring project

---
+++ src/test/java/com/talection/talection/TalectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talection.talection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
