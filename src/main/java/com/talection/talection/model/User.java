7704242a2d19590edf261a3405ed55bb8cced509
Kristian <garderkristian@gmail.com>
2025-06-09 11:15:18 +0200

    Fixed several compilation and import errors

--- src/main/java/com/talection/talection/model/User.java
+++ src/main/java/com/talection/talection/model/User.java
@@ -1,4 +1,30 @@
 package com.talection.talection.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "app_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String email;
+
+    public User() {
+        // Default constructor for JPA
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
