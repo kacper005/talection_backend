4941395227e6781f160ed90f3aab0732a6e93ef8
Kristian <garderkristian@gmail.com>
2025-06-13 10:09:39 +0200

    Implemented global exception handler for improved security

---
+++ src/main/java/com/talection/talection/config/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.talection.talection.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", Instant.now().toEpochMilli());
+        response.put("status", HttpStatus.FORBIDDEN.value());
+        response.put("error", "Forbidden");
+        response.put("message", "Access Denied");
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+}
